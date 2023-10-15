9508692878f1fa697d64629521b8639688d7b785
YisiHuang <20095257@mail.wit.ie>
2023-10-15 23:42:02 +0100

    Add to favorites logic completed; Dummy favorited page.

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -22,6 +22,13 @@ const genreId = Number(genreFilter);
     else setGenreFilter(value);
   };
 
+  const addToFavorites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favorite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
+
 const MovieListPage = (props) => {
   const [movies, setMovies] = useState([]);
 
@@ -52,7 +59,7 @@ const MovieListPage = (props) => {
       genreFilter={genreFilter}
     />
         </Grid>
-        <MovieList movies={displayedMovies} />
+        <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
       </Grid>
     </Grid>
   );

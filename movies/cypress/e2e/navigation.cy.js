f7d7c3c23d31c129ce325934dc9645d2af0821c9
YisiHuang <20095257@mail.wit.ie>
2023-10-27 15:03:42 +0100

    E2E tests - Exercise - Navigating between Favourites page and movie details page.

--- movies/cypress/e2e/navigation.cy.js
+++ movies/cypress/e2e/navigation.cy.js
@@ -49,7 +49,15 @@ describe("Navigation", () => {
     );
   });
   describe("From the favourites page to a movie's details", () => {
-    // TODO
+    beforeEach(() => {
+        // Select two favourites and navigate to Favourites page
+        cy.get("button[aria-label='add to favorites']").eq(0).click();
+        cy.get("button").contains("Favorites").click();
+    });
+    it("navigates to the movie details page and change browser URL", () => {
+        cy.get(".MuiCardActions-root").eq(0).contains("More Info").click();
+        cy.url().should("include", `/movies/${movies[0].id}`);
+    });
   });
   describe("The forward/backward links", () => {
     beforeEach(() => {
